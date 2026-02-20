4b12a2ff5b21dac638f196975b8d7a5a2a37d99e
vignshh7 <vigneshdark07@gmail.com>
2026-02-20 15:34:07 +0530

    backend fix

--- backend/src/app.js
+++ backend/src/app.js
@@ -15,7 +15,11 @@ const allowedOrigins = [
 app.use(
 	cors({
 		origin: (origin, callback) => {
-			if (!origin || allowedOrigins.includes(origin)) {
+			if (
+				!origin ||
+				allowedOrigins.includes(origin) ||
+				(origin && origin.endsWith(".vercel.app"))
+			) {
 				return callback(null, true);
 			}
 			return callback(new Error("Not allowed by CORS"));
